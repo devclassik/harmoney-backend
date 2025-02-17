fe564d4bb4fb99e644d115cda95872c9607ec697
Isaac <dev.iceztech@gmail.com>
2025-02-17 08:05:55 +0100

    Created enums for entity column types

---
+++ src/database/enum.ts
@@ -0,0 +1,16 @@
+export enum PhoneTypes {
+  HOME = 'HOME',
+  WORK = 'WORK',
+  CELL = 'CELL',
+}
+
+export enum Genders {
+  MALE = 'MALE',
+  FEMALE = 'FEMALE',
+}
+
+export enum EmployeeStatus {
+  ACTIVE = 'ACTIVE',
+  INACTIVE = 'INACTIVE',
+  SUSPENDED = 'SUSPENDED',
+}
