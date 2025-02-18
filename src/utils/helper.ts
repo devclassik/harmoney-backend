926c63404c3e20ff4e0151ba7d2b389bf0b00086
Isaac <dev.iceztech@gmail.com>
2025-02-18 14:29:24 +0100

    Added an helper function

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -3,3 +3,13 @@ export const capitalizeFirst = (str?: string): string => {
 
   return str.replace(/^./, (char) => char.toUpperCase());
 };
+
+export function getNumberOfDays(startDate: string, endDate: string): number {
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+
+  const diffInTime = end.getTime() - start.getTime();
+  const diffInDays = diffInTime / (1000 * 60 * 60 * 24);
+
+  return Math.ceil(diffInDays);
+}
