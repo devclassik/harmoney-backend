c6d15ad9f70a3cb8c3f1a26f530210b26ae2bbc5
Isaac <dev.iceztech@gmail.com>
2025-02-19 08:44:17 +0100

    Created the FileIndex entity

---
+++ src/database/entity/FileIndex.ts
@@ -0,0 +1,44 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Leave } from '.';
+import { DocumentTypes } from '../enum';
+
+@Entity()
+export class FileIndex {
+  constructor(data: FileIndex) {
+    Object.assign(this, data);
+  }
+
+  @PrimaryGeneratedColumn()
+  id?: number;
+
+  @Column({ type: 'varchar', nullable: true })
+  documentId?: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  name?: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  downloadUrl?: string;
+
+  @Column({ type: 'enum', enum: DocumentTypes, default: DocumentTypes.PDF })
+  type?: DocumentTypes;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt?: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt?: Date;
+
+  @DeleteDateColumn({ type: 'timestamp' })
+  deletedAt?: Date;
+}
